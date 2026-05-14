66cc281b40cc155b4fc793ea10518e4948eec6b3
Ako3lova <a.ko3lova@gmail.com>
2026-05-14 19:41:40 +0300

    Была добвлена связь фронта с бэком на страницах регистрации и логина. Обновлены файлы Register.jsx и Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,39 @@
 import "./Login.css"
 import { useNavigate } from "react-router-dom"
+import React, { useState } from 'react';
 
 function Login() {
     const navigate = useNavigate()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("") 
+
+    async function startLogin() {
+        setError("") 
+
+        const response = await fetch("http://localhost:8000/login", { 
+            method: "POST", 
+            headers: { "Content-Type": "application/json" }, 
+            body: JSON.stringify({ email, password }) 
+        })
+
+        const data = await response.json()
+
+        if (response.ok) {
+            console.log("Вход выполнен!", data)   
+            localStorage.setItem("token", data.access_token) 
+            navigate("/profile") 
+        } else {
+            setError(data.detail || "Ошибка входа")
+        }
+    }
     return (
         <div className="Login">
             <div className="login-frame">
                 <h1>Вход</h1>
-                <input type="email" placeholder="Почта" />
-                <input type="password" placeholder="Пароль" />
-                <button className="login-btn" onClick={() => navigate("/profile")}>Войти</button>
+                <input type="email" placeholder="Почта" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input type="password" placeholder="Пароль" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <button className="login-btn" onClick={startLogin}>Войти</button>
                 <p className="login-link" onClick={() => navigate("/register")}>
                     Нет аккаунта? <span>Зарегистрироваться</span>
                 </p>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,16 +1,51 @@
 import "./Register.css"
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"; //Объявления для useState, иначе ошибка
 
 function Register(){
     const navigate = useNavigate()
+    //Создание переменной email со значением "" и функции, которая изменяет этот email
+    //Аналогично с password
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const [error, setError] = useState("") //Состояние ошибки, для пользователя!
+
+    async function startRegistration(){
+      
+        const response = await fetch("http://localhost:8000/registrated", { //Отправление запроса, не двигаемся дальше,если не вернулся ответ
+            method: "POST", //передача серверу данных
+            headers: { "Content-Type": "application/json" }, //Передаем инфу, что внутри лежит формат JSON
+            body: JSON.stringify({ email, password }) //Передача переменных и сплющивание их в единую фтроку формата JSON
+    });
+    //Убираем navigate("/profile") напрямую для кнопки "Созадть аккаунт"
+    //Проверка, чтобы избежать смены страницы до того, как пользователь попал в бд 
+    const data = await response.json(); //Превращаем ответ сервера в понятный объект
+
+    if (response.ok) {
+        console.log("Регистрация прошла успешно!",data);
+        navigate("/login"); //Как раз из кнопки перекинули сюда, в проверку
+    } else {
+        console.log("Ошибка сервера:", data);
+        setError(data.detail || "Произошла ошибка при регистрации");
+    }
+
+    console.log("Запрос улетел, ответ получен!");
+}
+    
+
     return(
         <div className="Register">
             <div className="frame">
                 <h1>Регистрация</h1>
-                <input type="email" placeholder="Почта" />
-                <input type="password" placeholder="Пароль" />
+                {/*Если вдруг существует error, выведем для пользователя инфу об ошибке*/}
+                {error && <p style={{ color: "red", fontSize: "14px", marginBottom: "10px" }}>{error}</p>} 
+
+                {/*Создание объекта события(е),нахождения объекта которые его вызвал(target),и достать оттуда value. */}
+                {/*Onchange срабатывает каждый раз при вводе символа. Новый символ - вызов onChange */}
+                <input type="email" placeholder="Почта" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input type="password" placeholder="Пароль" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 <input type="password" placeholder="Повторите пароль" />
-                <button className="but2" onClick={() => navigate("/profile")}>Создать аккаунт</button>
+                <button className="but2" onClick={startRegistration}>Создать аккаунт</button>
                 <p className="link" onClick={() => navigate("/login")}>Уже есть аккаунт? <span>Войти</span></p>
             </div>
         </div>
